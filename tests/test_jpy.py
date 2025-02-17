a472a669a522a6ed4b3d52599eb0d706de803a92
Valmir França <vfranca3@gmail.com>
2025-02-17 11:59:16 -0300

    Adicionados testes para jpy

---
+++ tests/test_jpy.py
@@ -0,0 +1,14 @@
+from click.testing import CliRunner
+from cur import cur
+
+run = CliRunner()
+
+
+def test_calcula_conversao_jpy_para_usd():
+    res = run.invoke(cur, ["jpy", "300"])
+    assert res.output == "2.07 USD\n"
+
+
+def test_calcula_conversao_jpy_para_usd_com_opcao_cambio():
+    res = run.invoke(cur, ["jpy", "--cambio", "152", "300"])
+    assert res.output == "1.97 USD\n"
