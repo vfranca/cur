651ee257e11ce133240382c8cbaa94702d46e656
Valmir França <vfranca3@gmail.com>
2025-02-17 06:05:26 -0300

    Adicionado teste de conversao brl para usd com cambio opcao

--- tests/test_brl.py
+++ tests/test_brl.py
@@ -7,3 +7,8 @@ run = CliRunner()
 def test_calcula_conversao_brl_para_usd():
     res = run.invoke(cur, ["brl", "55"])
     assert res.output == "9.82 USD\n"
+
+
+def test_calcula_conversao__brl_para_usd_com_opcao_cambio():
+    res = run.invoke(cur, ["brl", "--cambio", "5.71", "55"])
+    assert res.output == "9.63 USD\n"
